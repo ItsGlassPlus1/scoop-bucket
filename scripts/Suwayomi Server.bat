088634dc76bf0b7dbd3c58314bc45d5b9d54ebf0
ItsGlassPlus1 <183306390+ItsGlassPlus1@users.noreply.github.com>
2025-12-30 12:21:42 -0800

    Update Suwayomi Server.bat to show the terminal window

--- scripts/Suwayomi Server.bat
+++ scripts/Suwayomi Server.bat
@@ -1,1 +1,1 @@
-start "" jre/bin/javaw --add-exports=java.desktop/sun.awt=ALL-UNNAMED -jar Suwayomi-Launcher.jar --launch
+start "" jre/bin/java --add-exports=java.desktop/sun.awt=ALL-UNNAMED -jar bin/Suwayomi-Server.jar
